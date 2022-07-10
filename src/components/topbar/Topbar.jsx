97deebb46fce08a92594d43cd1c73ea2cc74dc84
hoshico <melancholinista.yasu@gmail.com>
2022-07-11 07:14:56 +0900

    install materialUi

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,22 +1,24 @@
+import { Chat, Notifications, Search } from '@mui/icons-material'
 import React from 'react'
 
 export default function Topbar() {
   return (
-    <div className='topbarcontainer'>
+    <div className="topbarcontainer">
       <div className="topbarLeft">
         <span className='logo'>Real SNS</span>
       </div>
       <div className="searchbar">
+        <Search className="searchIcon" />
         <input type="text" className='searchInput' placeholder='探し物はなんですか?'/>
       </div>
       <div className="topbarRight">
         <div className="topbarIconItem">
-          1
+          <Chat />
+          <span className="topbarIconBadge">1</span>
         </div>
-        <div className="topbarIconItem">
-          2
+          <Notifications />
+          <span className="topbarIconBadge">2</span>
         </div>
-      </div>
       <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
     </div>
   )
