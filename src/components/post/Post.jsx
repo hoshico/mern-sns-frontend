ca1d1111138899131a6ad1bdb2da423060d60738
hoshico <melancholinista.yasu@gmail.com>
2022-07-16 17:05:13 +0900

    add like function

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,10 +1,26 @@
 import { MoreVert } from '@mui/icons-material';
-import React from 'react';
+import React, { useState } from 'react';
 import "./Post.css";
 import { Users } from "../../dummyData";
 
 export default function Post({ post }) {
-  //const user = Users.filter((user) => user.id === 1);
+  // useStateの初期値でpost.like(いいねの数)をセット
+  const [like, setLike] = useState(post.like);
+  // likeを既に押しているかのフラグ
+  const [isLiked, setIsliked] = useState(false);
+
+  const handleLike = () => {
+    /*
+      isLiked(いいねを押したかどうか)は初期値はfalse
+      クリックでset関数で更新
+
+      like(いいねの数)は初期値はpost.like
+      クリックで"isLiked"がtrue(ずでに押している)なら -1
+      いいねしていなければ +1 する
+    */
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsliked(!isLiked);
+  };
   return (
     <div className='post'>
       <div className="postWrapper">
@@ -24,8 +40,8 @@ export default function Post({ post }) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img src="./assets/heart.png" alt="" className='likeIcon'/>
-            <span className="postLikeCounter">{post.like}人がいいねを押しました</span>
+            <img src="./assets/heart.png" alt="" className='likeIcon' onClick={() => handleLike()}/>
+            <span className="postLikeCounter">{like}人がいいねを押しました</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentCounter">{post.comment}:コメント</span>
